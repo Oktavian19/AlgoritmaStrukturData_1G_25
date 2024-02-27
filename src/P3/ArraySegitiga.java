8e44f781f7156dbaf6b84e6c97fd5bc0394d2935
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-02-27 21:35:08 +0700

    P3_percobaan3_pertanyaan5_25

--- src/P3/ArraySegitiga.java
+++ src/P3/ArraySegitiga.java
@@ -10,5 +10,9 @@ public class ArraySegitiga {
         for (int i = 0; i < sgArray.length; i++) {
             sgArray[i] = new Segitiga(alas[i], tinggi[i]);
         }
+
+        for (int i = 0; i < sgArray.length; i++) {
+            System.out.printf("Segitiga ke-%d memiliki luas %d dan keliling %.2f\n", i, (int)sgArray[i].hitungLuas(), sgArray[i].hitungKeliling());
+        }
     }
 }
