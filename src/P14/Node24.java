3350a0213ad00b9b055a466bb4dddc5b7e643e40
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-05-30 19:29:29 +0700

    P11_percobaan1_24

---
+++ src/P14/Node24.java
@@ -0,0 +1,13 @@
+package P14;
+
+public class Node24 {
+    int data;
+    Node24 left;
+    Node24 right;
+
+    Node24(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
