4da76ed266a2fcaa268cb6e4256f4e88911c320d
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-06-06 21:55:44 +0700

    P12_percobaan2_24

--- src/P15/GraphMain24.java
+++ src/P15/GraphMain24.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class GraphMain24 {
     public static void main(String[] args) throws Exception {
+        /*
         Graph24 gedung = new Graph24(6);
         gedung.addEdge(0, 1, 50);
         gedung.addEdge(0, 2, 100);
@@ -18,6 +19,19 @@ public class GraphMain24 {
         checkAdjacency(gedung);
         System.out.println("");
         checkAdjacency(gedung);
+        */
+
+        GraphMatriks24 gdg = new GraphMatriks24(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
     }
 
     static void checkAdjacency(Graph24 check) throws Exception{
---
+++ src/P15/GraphMatriks24.java
@@ -0,0 +1,31 @@
+package P15;
+
+public class GraphMatriks24 {
+    int vertex;
+    int[][] matriks;
+
+    GraphMatriks24(int v) {
+        vertex = v;
+        matriks = new int[v][v];
+    }
+
+    void makeEdge(int asal, int tujuan, int jarak) {
+        matriks[asal][tujuan] = jarak;
+    }
+
+    void removeEdge(int asal, int tujuan) {
+        matriks[asal][tujuan] = 0;
+    }
+
+    void printGraph() {
+        for (int i = 0; i < vertex; i++) {
+            System.out.print("Gedung " + (char) ('A' + i) + ": ");
+            for (int j = 0; j < vertex; j++) {
+                if (matriks[i][j] != -1) {
+                    System.out.print("Gedung " + (char) ('A' + j) + " (" + matriks[i][j] + " m), ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
